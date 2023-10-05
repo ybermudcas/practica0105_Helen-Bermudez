03449310d73327ae21c49d0597dc79bb8d870edc
ybermudcas <ybermudcas@educacion.navarra.es>
2023-10-05 16:42:13 +0200

    Update ejercicio 4.py

--- ejercicio 4.py
+++ ejercicio 4.py
@@ -1,3 +1,3 @@
 n=int(input("Introduce un número entero positivo"))
 calcular=n*n+1/2
-print("suma de los  primeros enteros positivos"+n+calcular)
+print("suma de los  primeros enteros positivos"+n+calcular) 
