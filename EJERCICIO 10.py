dc17f31036a72b01ca1adb4fc1eee0fa6ce05cdb
ybermudcas <ybermudcas@educacion.navarra.es>
2023-10-06 19:16:55 +0200

    EJERCICIO 10

---
+++ EJERCICIO 10.py
@@ -0,0 +1,6 @@
+pesop=112
+pesom=75
+payasos = (int(input("escribe la cantidad de payasos vendidos")))
+muñecas = (int(input("escrube la cantidad de muñecas vendidas")))
+pesodelpaquete = (pesop*payasos+pesom*muñecas)
+print("peso total del paquete que será enviado es", pesodelpaquete "g")
