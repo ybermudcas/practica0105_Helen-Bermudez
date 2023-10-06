9d835121af5b6a5239d1326f49b3480951fcd063
ybermudcas <ybermudcas@educacion.navarra.es>
2023-10-06 20:50:52 +0200

    EJERCICIO 9

---
+++ EJERCICIO 9.py
@@ -0,0 +1,4 @@
+cantidad = (float(input("¿Qué cantidad quieres invertir?")))
+porcentaje = (float(input("¿Qué interés porcentual quieres?")))
+años =  (float(input("Por cuántos años?")))
+print ("capital obtenido en la inversión" , (cantidad * (porcentaje / 100) ** años ))
